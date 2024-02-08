0cb8a0da3a266494fb070caf3952b6562c546340
unknown <alvarosgd97@gmail.com>
2024-02-08 00:48:58 -0300

    solucion

--- server/app.js
+++ server/app.js
@@ -9,12 +9,13 @@ class Server {
   }
 
   middlewares() {
-    app.use((req, res, next) => {
+    this.app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*'); // Permite el acceso desde cualquier origen
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Encabezados permitidos
-      next();
-    });
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Encabezados permitidos
+  next();
+});
+    
 
     this.app.use(express.json());
     this.app.use(morgan("dev"));
